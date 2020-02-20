a653be560ce3043eb141df163272e5b5b273d110
citizen-dror <reshef.dror@gmail.com>
2020-02-20 11:51:29 +0200

    client build - basic filters and table

---
+++ build/precache-manifest.b2c3d10e872b47e9a818480d50607efa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6282dbba6acc7ae6a97be0d02b8f7fd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "435230fac84b5ca3457f",
+    "url": "/static/css/2.dd131328.chunk.css"
+  },
+  {
+    "revision": "38eb6a4668e10f6a3a6c",
+    "url": "/static/css/main.62374280.chunk.css"
+  },
+  {
+    "revision": "435230fac84b5ca3457f",
+    "url": "/static/js/2.bebb42b4.chunk.js"
+  },
+  {
+    "revision": "38eb6a4668e10f6a3a6c",
+    "url": "/static/js/main.68e3ef90.chunk.js"
+  },
+  {
+    "revision": "bb3db1b794bd9325906d",
+    "url": "/static/js/runtime-main.96118375.js"
+  }
+]);
