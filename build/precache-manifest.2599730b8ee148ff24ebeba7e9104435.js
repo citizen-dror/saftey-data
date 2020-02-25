d20f64f40df483f56cdcc058d9593d4bfc1b3df5
citizen-dror <reshef.dror@gmail.com>
2020-02-25 18:53:41 +0200

    client with localiztion

---
+++ build/precache-manifest.2599730b8ee148ff24ebeba7e9104435.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277dc62b625679962bba8f838585dbd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0bd81dc3a5e2b9c4b6c",
+    "url": "/static/css/2.e53ae49d.chunk.css"
+  },
+  {
+    "revision": "a2624a49a809a5858899",
+    "url": "/static/css/main.9f5acd38.chunk.css"
+  },
+  {
+    "revision": "b0bd81dc3a5e2b9c4b6c",
+    "url": "/static/js/2.47db0512.chunk.js"
+  },
+  {
+    "revision": "a2624a49a809a5858899",
+    "url": "/static/js/main.df03b7f4.chunk.js"
+  },
+  {
+    "revision": "bb3db1b794bd9325906d",
+    "url": "/static/js/runtime-main.96118375.js"
+  }
+]);
