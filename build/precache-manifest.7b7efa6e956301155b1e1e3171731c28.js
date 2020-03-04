f63082f0abccaa8c3d79d0836a2c256d4e158e31
citizen-dror <reshef.dror@gmail.com>
2020-03-04 08:30:27 +0200

    clinet with grpah and tabs

---
+++ build/precache-manifest.7b7efa6e956301155b1e1e3171731c28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80594a8a5ae2ceca0b9366bcf57816e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa33b8fd5ae4b4ea0bd5",
+    "url": "/static/css/2.e53ae49d.chunk.css"
+  },
+  {
+    "revision": "99cb9200c5d7cfce6204",
+    "url": "/static/css/main.0233f2c0.chunk.css"
+  },
+  {
+    "revision": "aa33b8fd5ae4b4ea0bd5",
+    "url": "/static/js/2.74b160f8.chunk.js"
+  },
+  {
+    "revision": "99cb9200c5d7cfce6204",
+    "url": "/static/js/main.173c11c6.chunk.js"
+  },
+  {
+    "revision": "bb3db1b794bd9325906d",
+    "url": "/static/js/runtime-main.96118375.js"
+  }
+]);
