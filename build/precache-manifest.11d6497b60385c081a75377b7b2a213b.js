554e7e1f2aecdba3d3c23adbf13e7b81849dd9ca
citizen-dror <reshef.dror@gmail.com>
2020-03-10 18:16:58 +0200

    client filter road-segment

---
+++ build/precache-manifest.11d6497b60385c081a75377b7b2a213b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc90db5c10493b39bf5997c7957b2888",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5c7c49faf94e2967f06",
+    "url": "/static/css/2.e53ae49d.chunk.css"
+  },
+  {
+    "revision": "1cacc2c44e558312b328",
+    "url": "/static/css/main.ec2c0496.chunk.css"
+  },
+  {
+    "revision": "b5c7c49faf94e2967f06",
+    "url": "/static/js/2.cbaa6bd8.chunk.js"
+  },
+  {
+    "revision": "1cacc2c44e558312b328",
+    "url": "/static/js/main.d55770fe.chunk.js"
+  },
+  {
+    "revision": "bb3db1b794bd9325906d",
+    "url": "/static/js/runtime-main.96118375.js"
+  }
+]);
