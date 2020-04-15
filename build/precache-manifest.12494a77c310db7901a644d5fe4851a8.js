b956be902d07002cbe179f2796c1ca4b3d459825
citizen-dror <reshef.dror@gmail.com>
2020-04-15 10:07:55 +0300

    client tabs mountOnEnter and lazy-load

---
+++ build/precache-manifest.12494a77c310db7901a644d5fe4851a8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efdc7fabf46de1b157db0528ab06f49b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fc3d4f94b77a8ae6a81",
+    "url": "/static/css/0.c297138a.chunk.css"
+  },
+  {
+    "revision": "14a666b1cb2328e83cd4",
+    "url": "/static/css/3.e31a430a.chunk.css"
+  },
+  {
+    "revision": "ef2668873f4181826c08",
+    "url": "/static/css/5.01c246b6.chunk.css"
+  },
+  {
+    "revision": "16125facd1ed1920774d",
+    "url": "/static/css/main.15c2cee4.chunk.css"
+  },
+  {
+    "revision": "3fc3d4f94b77a8ae6a81",
+    "url": "/static/js/0.a4f7de63.chunk.js"
+  },
+  {
+    "revision": "83159567db5bfc107d2b",
+    "url": "/static/js/10.b5859ab8.chunk.js"
+  },
+  {
+    "revision": "82915b424397cde288ba",
+    "url": "/static/js/11.790fe4e1.chunk.js"
+  },
+  {
+    "revision": "14a666b1cb2328e83cd4",
+    "url": "/static/js/3.267a8fc2.chunk.js"
+  },
+  {
+    "revision": "8a3d345beb1899f1dcac",
+    "url": "/static/js/4.5f38c83f.chunk.js"
+  },
+  {
+    "revision": "ef2668873f4181826c08",
+    "url": "/static/js/5.897653d3.chunk.js"
+  },
+  {
+    "revision": "4675f870abd16b5afc62",
+    "url": "/static/js/6.f12459eb.chunk.js"
+  },
+  {
+    "revision": "d010cf483ec5e1327355",
+    "url": "/static/js/7.fed2ceba.chunk.js"
+  },
+  {
+    "revision": "321412d37fded78ea196",
+    "url": "/static/js/8.cc201857.chunk.js"
+  },
+  {
+    "revision": "37b860de8d8d18313bae",
+    "url": "/static/js/9.499d3c74.chunk.js"
+  },
+  {
+    "revision": "16125facd1ed1920774d",
+    "url": "/static/js/main.e17a34cf.chunk.js"
+  },
+  {
+    "revision": "bb8cc0b740203dd8aeff",
+    "url": "/static/js/runtime-main.9bf3bc4e.js"
+  }
+]);
