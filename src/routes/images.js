faa88dd170c1f72b0dfffd8256f1c8945a5c9fb9
citizen-dror <reshef.dror@gmail.com>
2020-04-16 07:35:20 +0300

    upload img to db and get it

--- src/routes/images.js
+++ src/routes/images.js
@@ -21,13 +21,46 @@ router.post('/', upload.single('image'), (req, res, next) => {
     if (!req.body) {
         return res.status(400).send("request boddy is missing!")
     }
-    console.log(req.body.title)
-    console.log(req.file)
-    //
-    // const newImg = new imgModel();
-    // newImg.img.data = fs.readFileSync(req.files.userPhoto.path)
-    // newImg.img.contentType = 'image / png';
-    // newImg.save();
-    res.send(`got image ${req.body.name}`)
+    //console.log(req.body.title)
+    console.log(req.file.originalname)
+    //console.log(req.file)
+    const newImg = new imgModel();
+    newImg.filename = req.file.originalname;
+    newImg.contentType = req.file.mimetype;
+    newImg.title = req.body.title;
+    newImg.data = fs.readFileSync('./uploads/' + req.file.originalname)
+    newImg.save();
+    res.send(`got image ${req.file.originalname}`)
 })
+
+router.get('/:filename', (req, res) => {
+    imgModel.findOne({ filename: req.params.filename }, (err, file) => {
+      // Check if file
+      if (!file || file.length === 0) {
+        return res.status(404).json({
+          err: 'No file exists',
+        })
+      }
+      // Check if image
+      if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+        // Read output to browser
+        //const readstream = gfs.createReadStream(file.filename)
+        //readstream.pipe(res)
+        try{
+            fs.writeFileSync('./uploads/' + 'tmp-jsa-header.png', file.data) 
+            //fs.writeFileSync(__dirname + '/static/assets/tmp/tmp-jsa-header.png', file.data);
+            //console.log("Stored an image 'tmp-jsa-header.png' in '/static/assets/tmp' folder.");
+            res.send(`got image ${file.filename}`)
+          }catch(e){
+            console.log(e);
+          }
+
+      } else {
+        res.status(404).json({
+          err: 'Not an image',
+        })
+      }
+    })
+  })
+
 module.exports = router
