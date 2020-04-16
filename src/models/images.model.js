bb5c659ab06a180cd0c99193929dfd79c8dd09ce
citizen-dror <reshef.dror@gmail.com>
2020-04-16 07:35:20 +0300

    upload img to db and get it

--- src/models/images.model.js
+++ src/models/images.model.js
@@ -1,7 +1,9 @@
 let mongoose  = require ('mongoose')
 
-let ImageSchema = mongoose.Schema(
-    { img: { data: Buffer, contentType: String }
-    }
-)
+let ImageSchema = mongoose.Schema({ 
+    filename:String,
+    contentType: String,
+    title: String,
+    data: Buffer
+})
 module.exports = mongoose.model('Image', ImageSchema)
