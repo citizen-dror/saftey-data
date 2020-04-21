f29cfe64c7ec1895543b8124b3a9dba5c9b7b77b
citizen-dror <reshef.dror@gmail.com>
2020-04-21 11:28:04 +0300

    client - static data for grpahs

---
+++ build/precache-manifest.5360a82a7f9790d3956ae14ca3cd3b69.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a4939a2dd6e95ff1a06b9b25e70422",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31d63bd4eae74db6f177",
+    "url": "/static/css/0.c297138a.chunk.css"
+  },
+  {
+    "revision": "0fa82a2a568784d3d132",
+    "url": "/static/css/3.e31a430a.chunk.css"
+  },
+  {
+    "revision": "bf551582438d6b45a777",
+    "url": "/static/css/5.01c246b6.chunk.css"
+  },
+  {
+    "revision": "550a7592cfd408b715ee",
+    "url": "/static/css/main.15c2cee4.chunk.css"
+  },
+  {
+    "revision": "31d63bd4eae74db6f177",
+    "url": "/static/js/0.5127c656.chunk.js"
+  },
+  {
+    "revision": "b9c6b880fe8544aa3f81",
+    "url": "/static/js/10.29ad14c2.chunk.js"
+  },
+  {
+    "revision": "ab5ff57c661d60ce1f7d",
+    "url": "/static/js/11.faa79540.chunk.js"
+  },
+  {
+    "revision": "0fa82a2a568784d3d132",
+    "url": "/static/js/3.61684918.chunk.js"
+  },
+  {
+    "revision": "c74b3e7bb0768f0f417a",
+    "url": "/static/js/4.9e059cd2.chunk.js"
+  },
+  {
+    "revision": "bf551582438d6b45a777",
+    "url": "/static/js/5.2b8ce37c.chunk.js"
+  },
+  {
+    "revision": "e63fbed0af7416390568",
+    "url": "/static/js/6.e3859a40.chunk.js"
+  },
+  {
+    "revision": "0094ee4fe91287f189eb",
+    "url": "/static/js/7.40975eeb.chunk.js"
+  },
+  {
+    "revision": "79edb852e2466d096d79",
+    "url": "/static/js/8.dd677eb9.chunk.js"
+  },
+  {
+    "revision": "0ae4ac6cbb4329885dcc",
+    "url": "/static/js/9.920aa532.chunk.js"
+  },
+  {
+    "revision": "550a7592cfd408b715ee",
+    "url": "/static/js/main.d003fa24.chunk.js"
+  },
+  {
+    "revision": "00cf1dc78495447396ed",
+    "url": "/static/js/runtime-main.782407a0.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.12494a77c310db7901a644d5fe4851a8.js"
+  "/precache-manifest.5360a82a7f9790d3956ae14ca3cd3b69.js"
 );
 
 self.addEventListener('message', (event) => {
