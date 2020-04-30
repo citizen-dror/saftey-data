9401226762637f4691f8cadbd8fddc1064307b06
citizen-dror <reshef.dror@gmail.com>
2020-04-30 16:26:49 +0300

    disable uploadimage

--- src/routes/images.js
+++ src/routes/images.js
@@ -77,103 +77,103 @@ const getProjectionByLang = (lang) => {
 //   return res.status(500).jsonp(err);
 // };
 
-router.post('/', upload.single('image'), (req, res, next) => {
-  if (!req.body) {
-    return res.status(400).send('request boddy is missing!');
-  }
-  // console.log(req.body.title)
-  console.log(req.file.originalname);
-  // console.log(req.file)
-  const newImg = new ImgModel();
-  newImg.filename = req.file.originalname;
-  newImg.contentType = req.file.mimetype;
-  newImg.titlehe = req.body.titlehe;
-  newImg.texthe = req.body.texthe;
-  newImg.titleen = req.body.titleen;
-  newImg.texten = req.body.texten;
-  newImg.titlear = req.body.titlear;
-  newImg.textar = req.body.textar;
-  newImg.place = req.body.place;
-  newImg.index = req.body.index;
-  newImg.tags = req.body.tags.split(',');
-  newImg.data = fs.readFileSync(`./uploads/${req.file.originalname}`);
-  newImg.save((err) => {
-    if (err) {
-      next(err); // Pass errors to Express.
-    } else {
-      return res.send(`got image ${req.file.originalname}`);
-    }
-  });
-  return true;
-});
+// router.post('/', upload.single('image'), (req, res, next) => {
+//   if (!req.body) {
+//     return res.status(400).send('request boddy is missing!');
+//   }
+//   // console.log(req.body.title)
+//   console.log(req.file.originalname);
+//   // console.log(req.file)
+//   const newImg = new ImgModel();
+//   newImg.filename = req.file.originalname;
+//   newImg.contentType = req.file.mimetype;
+//   newImg.titlehe = req.body.titlehe;
+//   newImg.texthe = req.body.texthe;
+//   newImg.titleen = req.body.titleen;
+//   newImg.texten = req.body.texten;
+//   newImg.titlear = req.body.titlear;
+//   newImg.textar = req.body.textar;
+//   newImg.place = req.body.place;
+//   newImg.index = req.body.index;
+//   newImg.tags = req.body.tags.split(',');
+//   newImg.data = fs.readFileSync(`./uploads/${req.file.originalname}`);
+//   newImg.save((err) => {
+//     if (err) {
+//       next(err); // Pass errors to Express.
+//     } else {
+//       return res.send(`got image ${req.file.originalname}`);
+//     }
+//   });
+//   return true;
+// });
 
-router.put('/props/', async (req, res) => {
-  if (!req.body) {
-    return res.status(400).send('request boddy is missing!');
-  }
-  // console.log(req.body.title)
-  console.log(req.body.filename);
-  const filter = { filename: req.body.filename };
-  const update = {
-    titlehe: req.body.titlehe,
-    texthe: req.body.texthe,
-    titleen: req.body.titleen,
-    texten: req.body.texten,
-    titlear: req.body.titlear,
-    textar: req.body.textar,
-    place: req.body.place,
-    tags: req.body.tags,
-    index: req.body.index,
-  };
-  // `doc` is the document _after_ `update` was applied because of
-  // `new: true`
-  await ImgModel.findOneAndUpdate(filter, update, {
-    new: true,
-  });
-  // remove data from return answer to make it lighter
-  return res.status(200).jsonp('ok');
-});
+// router.put('/props/', async (req, res) => {
+//   if (!req.body) {
+//     return res.status(400).send('request boddy is missing!');
+//   }
+//   // console.log(req.body.title)
+//   console.log(req.body.filename);
+//   const filter = { filename: req.body.filename };
+//   const update = {
+//     titlehe: req.body.titlehe,
+//     texthe: req.body.texthe,
+//     titleen: req.body.titleen,
+//     texten: req.body.texten,
+//     titlear: req.body.titlear,
+//     textar: req.body.textar,
+//     place: req.body.place,
+//     tags: req.body.tags,
+//     index: req.body.index,
+//   };
+//   // `doc` is the document _after_ `update` was applied because of
+//   // `new: true`
+//   await ImgModel.findOneAndUpdate(filter, update, {
+//     new: true,
+//   });
+//   // remove data from return answer to make it lighter
+//   return res.status(200).jsonp('ok');
+// });
 
-router.put('/', upload.single('image'), async (req, res) => {
-  if (!req.body) {
-    return res.status(400).send('request boddy is missing!');
-  }
-  // console.log(req.body.title)
-  console.log(req.file.originalname);
-  const filter = { filename: req.file.originalname };
-  const update = {
-    titlehe: req.body.titlehe,
-    texthe: req.body.texthe,
-    titleen: req.body.titleen,
-    texten: req.body.texten,
-    titlear: req.body.titlear,
-    textar: req.body.textar,
-    place: req.body.place,
-    tags: JSON.parse(req.body.tags),
-    data: fs.readFileSync(`./uploads/${req.file.originalname}`),
-  };
-  // `doc` is the document _after_ `update` was applied because of
-  // `new: true`
-  const doc = await ImgModel.findOneAndUpdate(filter, update, {
-    new: true,
-  });
-  // remove data from return answer to make it lighter
-  delete doc.data;
-  return res.status(200).jsonp(doc);
-});
+// router.put('/', upload.single('image'), async (req, res) => {
+//   if (!req.body) {
+//     return res.status(400).send('request boddy is missing!');
+//   }
+//   // console.log(req.body.title)
+//   console.log(req.file.originalname);
+//   const filter = { filename: req.file.originalname };
+//   const update = {
+//     titlehe: req.body.titlehe,
+//     texthe: req.body.texthe,
+//     titleen: req.body.titleen,
+//     texten: req.body.texten,
+//     titlear: req.body.titlear,
+//     textar: req.body.textar,
+//     place: req.body.place,
+//     tags: JSON.parse(req.body.tags),
+//     data: fs.readFileSync(`./uploads/${req.file.originalname}`),
+//   };
+//   // `doc` is the document _after_ `update` was applied because of
+//   // `new: true`
+//   const doc = await ImgModel.findOneAndUpdate(filter, update, {
+//     new: true,
+//   });
+//   // remove data from return answer to make it lighter
+//   delete doc.data;
+//   return res.status(200).jsonp(doc);
+// });
 
-router.get('/tags/:lang/:tag', (req, res) => {
-  const { lang, tag } = req.params;
-  const projection = getProjectionByLang(lang);
-  const cond = { tags: tag };
-  ImgModel.find(cond, projection).sort({ index: 1 })
-    .then((doc) => {
-      prepareImages(doc);
-      return res.jsonp(doc);
-    })
-    .catch((err) => res.status(500).jsonp(err));
-  return true;
-});
+// router.get('/tags/:lang/:tag', (req, res) => {
+//   const { lang, tag } = req.params;
+//   const projection = getProjectionByLang(lang);
+//   const cond = { tags: tag };
+//   ImgModel.find(cond, projection).sort({ index: 1 })
+//     .then((doc) => {
+//       prepareImages(doc);
+//       return res.jsonp(doc);
+//     })
+//     .catch((err) => res.status(500).jsonp(err));
+//   return true;
+// });
 
 router.get('/tags/:tag', (req, res) => {
   const { tag } = req.params;
