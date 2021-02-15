c1584d4b0e89573cec25615feea5bc73dfd60f43
citizen-dror <reshef.dror@gmail.com>
2021-02-15 19:17:42 +0200

    add helmet lib, error handler

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const logger = require('./src/middlewares/logger');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(helmet());
+
 // eslint-disable-next-line no-unused-vars
 const db = require('./src/database');
 const usersRoute = require('./src/routes/users');
@@ -36,7 +41,11 @@ app.use((req, res) => {
   logger.info(`404 - ${req.originalUrl}`, req.body);
   res.status(404).send('The url you requested cannot be found.');
 });
-app.use((err) => {
-  logger.error(err.stack);
+app.use((error, req, res, next) => {
+  if (res.headersSent) {
+    return next(error);
+  }
+  logger.error(error.stack);
+  return res.status(500).send('Unknown Error in the server');
 });
 app.listen(PORT, () => logger.info(`Server has started on ${PORT}`));
