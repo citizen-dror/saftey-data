7558d7507167aef47ee3e65e9888cecc380f3f40
citizen-dror <reshef.dror@gmail.com>
2021-03-22 11:51:33 +0200

    scripts for heroku

--- dist/server.js
+++ dist/server.js
@@ -39,4 +39,4 @@ app.use((error, req, res, next) => {
 app.listen(port, () => {
     console.log(`server started ${port}`);
 });
-//# sourceMappingURL=app1.js.map
+//# sourceMappingURL=server.js.map
