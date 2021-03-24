311e389b26877d0663787a9a3d52b63bb617e9cc
citizen-dror <reshef.dror@gmail.com>
2021-03-24 15:35:49 +0200

    add imageService

---
+++ src/components/images/imageService.ts
@@ -0,0 +1,110 @@
+
+import fs from 'fs';
+import multer from 'multer';
+import path from 'path';
+import imageModel, { IImage } from './images.model';
+import logger from '../../middlewares/logger';
+
+const storage = multer.diskStorage({
+    destination: (req: any, file: any, cb: any) => {
+        cb(null, './../uploads/');
+    },
+    filename: (req: any, file: any, cb: any) => {
+        cb(null, file.originalname);
+    },
+});
+const upload = multer({ storage });
+
+const fetchImageFromDb = async (filename: string) => {
+    try {
+        const file: IImage = await imageModel.ImageModel.findOne({ filename }).exec();
+        if (!file) {
+            return (404); // 'No file exists'
+        }
+        if (file.contentType !== 'image/png' && file.contentType !== 'image/jpeg') {
+            logger.info('404 - Not an image');
+            return (404);
+        }
+        // logger.log(file.filename);
+        const filePath = path.join(__dirname, '/../../../uploads', `${file.filename}`);
+        fs.writeFileSync(filePath, file.data);
+        return (200);
+    } catch (err) {
+        console.log('fetchImageFromDb', err)
+        return 500;
+    }
+};
+
+const prepareImages = async (array: any[]) => {
+    array.forEach((element) => {
+        const filePath = `../uploads/${element.filename}`;
+        fs.access(filePath, async (err) => {
+            if (err) {
+                fetchImageFromDb(element.filename);
+            }
+        });
+    });
+    return null;
+};
+
+const countImages = async (query: any) => {
+    try {
+        const count = await imageModel.ImageModel.countDocuments(query).exec();
+        return count;
+    } catch (error) {
+        return -1;
+    }
+};
+
+const getProjectionByLang = (lang: string) => {
+    let projection = null;
+    let progLang = null;
+    if (lang === '') projection = { data: 0 };
+    else {
+        const baseProjection = {
+            _id: 1, filename: 1, contentType: 1, place: 1, tags: 1, index: 1, createdAt: 1,
+        };
+        if (lang === 'he') progLang = { titlehe: 1, texthe: 1 };
+        else if (lang === 'en') progLang = { titleen: 1, texten: 1 };
+        else progLang = { titlear: 1, textar: 1 };
+        projection = { ...baseProjection, ...progLang };
+    }
+    return projection;
+};
+
+const get_image_by_tag = async (lang: string, tag: string) => {
+    const projection = getProjectionByLang(lang);
+    const cond = { tags: tag };
+    const doc = await imageModel.ImageModel.find(cond, projection).sort({ index: 1 });
+    prepareImages(doc);
+    return doc;
+}
+
+const get_image_by_city = async (lang: string, city: string) => {
+    const projection = getProjectionByLang(lang);
+    const cond = { place: city };
+    const doc = await imageModel.ImageModel.find(cond, projection).sort({ index: 1 });
+    prepareImages(doc);
+    return doc;
+}
+
+const checkFileExists = async (file: string) => {
+    return fs.promises.access(file, fs.constants.F_OK)
+        .then(() => true)
+        .catch(() => false)
+}
+
+const get_image_file = async (filename: string) => {
+    const filePath = path.join(__dirname, '/../../../uploads', `${filename}`);
+    // console.log("get image ", filePath);
+    const foundFile = await checkFileExists(filePath);
+    if (foundFile === true) {
+        return { status: 200, file: filePath };
+    } else {
+        const status = await fetchImageFromDb(filename);
+        return { status: status, file: filePath };
+    }
+};
+
+
+export { get_image_file, get_image_by_tag, get_image_by_city, countImages, fetchImageFromDb, prepareImages, getProjectionByLang }
--- src/components/images/imagesAPI.ts
+++ src/components/images/imagesAPI.ts
@@ -1,125 +1,36 @@
 import { Router, Request, Response } from 'express';
-import fs from 'fs';
-import multer from 'multer';
-import path from 'path';
-import imageModel, {IImage} from './images.model';
-import logger from '../../middlewares/logger';
+
+import {
+  get_image_file,
+  get_image_by_tag,
+  get_image_by_city,
+  countImages,
+} from './imageService';
 
 const router = Router();
-
-const storage = multer.diskStorage({
-  destination: (req: any, file: any, cb: any) => {
-    cb(null, './../uploads/');
-  },
-  filename: (req: any , file: any, cb: any) => {
-    cb(null, file.originalname);
-  },
-});
-const upload = multer({ storage });
 
 const imageRoute = (app: Router) => {
   app.use('/api/v1/img', router);
 
-  //move to service
-  const fetchImageFromDb = async (filename: string) => {
-    try {
-      const file: IImage = await imageModel.ImageModel.findOne({ filename }).exec();
-      if (!file) {
-        return (404); // 'No file exists'
-      }
-      if (file.contentType !== 'image/png' && file.contentType !== 'image/jpeg') {
-        logger.info('404 - Not an image');
-        return (404);
-      }
-      // logger.log(file.filename);
-      const filePath = path.join(__dirname, '/../../../uploads',`${file.filename}`);
-      fs.writeFileSync(filePath, file.data);
-      return (200);
-    } catch (err) {
-      console.log ('fetchImageFromDb', err)
-      return 500;
-    }
-  };
-
-  const prepareImages = async (array: any[]) => {
-    array.forEach((element) => {
-      const filePath = `../uploads/${element.filename}`;
-      fs.access(filePath, async (err) => {
-        if (err) {
-          fetchImageFromDb(element.filename);
-        }
-      });
-    });
-    return null;
-  };
-  
-  const countImages = async (query: any) => {
-    try {
-      const count = await imageModel.ImageModel.countDocuments(query).exec();
-      return count;
-    } catch (error) {
-      return -1;
-    }
-  };
-  
-  const getProjectionByLang = (lang: string) => {
-    let projection = null;
-    let progLang = null;
-    if (lang === '') projection = { data: 0 };
-    else {
-      const baseProjection = {
-        _id: 1, filename: 1, contentType: 1, place: 1, tags: 1, index: 1, createdAt: 1,
-      };
-      if (lang === 'he') progLang = { titlehe: 1, texthe: 1 };
-      else if (lang === 'en') progLang = { titleen: 1, texten: 1 };
-      else progLang = { titlear: 1, textar: 1 };
-      projection = { ...baseProjection, ...progLang };
-    }
-    return projection;
-  };
-
   //controlers
-
-  router.get('/tags/:lang/:tag', (req, res) => {
+  router.get('/tags/:lang/:tag', async (req, res) => {
     const { lang, tag } = req.params;
-    const projection = getProjectionByLang(lang);
-    const cond = { tags: tag };
-    imageModel.ImageModel.find(cond, projection).sort({ index: 1 })
-      .then((doc) => {
-        prepareImages(doc);
-        return res.jsonp(doc);
-      })
-      .catch((err) => res.status(500).jsonp(err));
-    return true;
+    const doc = await get_image_by_tag(lang, tag);
+    return res.jsonp(doc);
   });
-  
-  router.get('/tags/:tag', (req, res) => {
-    const { tag } = req.params;
-    const projection = getProjectionByLang('');
-    const cond = { tags: tag };
-    imageModel.ImageModel.find(cond, projection).sort({ index: 1 })
-      .then((doc) => {
-        prepareImages(doc);
-        return res.jsonp(doc);
-      })
-      .catch((err) => res.status(500).jsonp(err));
-    return true;
+
+  router.get('/tags/:tag', async (req, res) => {
+    const { lang, tag } = req.params;
+    const doc = await get_image_by_tag('', tag);
+    return res.jsonp(doc);
   });
-  
-  
-  router.get('/place/:lang/:city', (req, res) => {
+
+  router.get('/place/:lang/:city', async (req, res) => {
     const { lang, city } = req.params;
-    const projection = getProjectionByLang(lang);
-    const cond = { place: city };
-    imageModel.ImageModel.find(cond, projection)
-      .then((doc) => {
-        prepareImages(doc);
-        res.jsonp(doc);
-      })
-      .catch((err) => res.status(500).jsonp(err));
-    return true;
+    const doc = await get_image_by_city(lang, city);
+    return res.jsonp(doc);
   });
-  
+
   router.get('/count/', async (req, res) => {
     const count = await countImages({});
     if (count >= 0) {
@@ -127,33 +38,20 @@ const imageRoute = (app: Router) => {
     }
     return res.status(500).json('error');
   });
-  
+
   router.get('/:filename', async (req, res) => {
-    const filePath = path.join(__dirname, '/../../../uploads',`${req.params.filename}`);
-    // console.log("get image ", filePath);
-    let foundFile = true;
-    await fs.access(filePath, async (err) => {
-      if (err) {
-        foundFile = false;
-        await fetchImageFromDb(req.params.filename)
-          .then(((status) => {
-            if (status === 200) {
-              return res.sendFile(filePath);
-            } if (status === 404) {
-              return res.status(status).json({
-                err: 'No file exists',
-              });
-            } 
-            {
-              console.log("No image file exists",status );
-              return res.status(500).json('eror on image fetch');
-            }
-          }));
-        // file exists
-      }
-      if (foundFile) return res.sendFile(filePath);
-    });
+    const {status, file }= await get_image_file(req.params.filename);
+    if (status === 200) {
+      return res.sendFile(file);
+    } else if (status === 404) {
+      return res.status(status).json({
+        err: 'No file exists',
+      });
+    } else {
+      console.log("No image file exists", status);
+      return res.status(500).json('eror on image fetch');
+    }
   });
-};
+}
 
 export default imageRoute;
